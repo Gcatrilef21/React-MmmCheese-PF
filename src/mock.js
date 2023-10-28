47907df7a77343487a515c9c74cffcf7be431fb2
Gonzalo Catrilef <catrilef.gonzalo@icloud.com>
2023-10-28 02:25:17 -0300

    update and test mock to fireStore

--- src/mock.js
+++ src/mock.js
@@ -1,11 +1,12 @@
-const cheeses = [{
+const cheeses = [
+{
     id: 1,
     image: '../images/gouda.png',
     title: 'Gouda',
     description: 'El queso Gouda es un queso de origen holandés conocido por su sabor suave y cremoso. Es versátil y se utiliza en una amplia variedad de platos, desde aperitivos hasta sándwiches y platos de queso fundido',
     price: 5990,
     stock: 6,
-    category: 'Quesos Semi-Duros'
+    category: 'Semi-Duros'
 },
 {
     id: 2,
@@ -22,7 +23,7 @@ const cheeses = [{
     description: 'El queso Maasdam es un queso de origen suizo-holandés que combina las mejores cualidades del queso suizo Emmental y el queso holandés Gouda. Es conocido por su distintivo sabor suave y ligeramente dulce, así como por su textura tierna y llena de agujeros, similar a la del Emmental.El queso Maasdam es versátil y se utiliza en una variedad de aplicaciones culinarias, desde platos de queso y sándwiches hasta ensaladas y fondues. ',
     stock: 15,
     price: 10990,
-    country: 'Quesos Semi-Duros'
+    category: 'Quesos Semi-Duros'
 }, {
     id: 4,
     image: '../images/brie.png',
@@ -44,8 +45,8 @@ const cheeses = [{
     image: '../images/roquefort.png',
     title: 'Roquefort',
     description: 'El queso Roquefort es un queso de origen francés famoso por su sabor intenso y característico, y por ser uno de los quesos azules más apreciados en todo el mundo.El Roquefort se disfruta comúnmente solo, en ensaladas, como parte de platos de queso o acompañado de peras, nueces u otros ingredientes que complementan su sabor.',
-    stock: 10,
     price: 23990,
+    stock: 10,
     category: 'Quesos Blandos'
 }, {
     id: 7,
@@ -54,7 +55,7 @@ const cheeses = [{
     description: 'Es un queso típico de Turquía, especialmente popular en la región de Anatolia. Este queso se elabora principalmente a partir de leche de oveja o una mezcla de leche de oveja y cabra,se consume en una variedad de platos y es un componente esencial en la cocina turca. ',
     price: 14990,
     stock: 5,
-    category: 'Quesos Extra Duros'
+    category: 'Extra Duros'
 }, {
     id: 8,
     image: '../images/emmental.png',
@@ -62,7 +63,7 @@ const cheeses = [{
     description: 'El queso Emmental es un queso suizo de renombre internacional que se destaca por sus característicos agujeros u "ojos".',
     price: 9990,
     stock: 12,
-    category: 'Quesos Semi-Duros'
+    category: 'Semi-Duros'
 }, {
     id: 9,
     image: '../images/appenzeller.png',
@@ -70,23 +71,23 @@ const cheeses = [{
     description: 'El queso Appenzeller es un queso suizo que se produce en la región de Appenzell, en el noreste de Suiza.Se puede disfrutar solo, en sándwiches, ensaladas o platos de queso fundido, y se considera una joya de la tradición quesera suiza',
     price: 15990,
     stock: 5,
-    category: 'Quesos Semi-Duros'
+    category: 'Semi-Duros'
 }, {
     id: 10,
     image: '../images/cheddar.png',
     title: 'Cheddar',
     description: 'Su nombre proviene de la región de Cheddar, en el suroeste de Inglaterra, donde se originó. Se caracteriza por su sabor robusto y su textura firme y desmenuzable cuando está maduro.Este queso es muy versátil y se utiliza en una amplia gama de aplicaciones culinarias, desde sándwiches y hamburguesas hasta ensaladas y platos de macarrones con queso.',
-    stock: 20,
     price: 14990,
-    category: 'Quesos Blandos'
+    stock: 20,
+    category: 'Blandos'
 }, {
     id: 11,
     image: '../images/stilton.png',
     title: 'Stilton',
     description: 'El queso Stilton es un queso azul de origen inglés que se destaca por su sabor distintivo y su textura cremosa con vetas de moho azul.Este queso se consume comúnmente solo, acompañado de galletas o pan, pero también se utiliza en la cocina para preparar salsas, aderezos de ensaladas y platos de queso fundido. ',
-    stock: 8,
     price: 15990,
-    category: 'Quesos Extra Duro'
+    stock: 8,
+    category: 'Extra Duro'
 }, {
     id: 12,
     image: '../images/feta.png',
@@ -94,7 +95,7 @@ const cheeses = [{
     description: 'El queso Feta es de origen griego, conocido por su sabor salado y ligeramente ácido, así como por su textura quebradiza y húmeda.La versatilidad y el sabor distintivo del queso Feta lo han convertido en un elemento fundamental en la gastronomía de muchas culturas y regiones.',
     price: 5990,
     stock: 8,
-    category: 'Quesos Semi-Duros'
+    category: 'Semi-Duros'
 }
 ]
 
@@ -107,7 +108,7 @@ const category = [{
     name: 'Quesos Semi-Duros'
 },
 {
-    id: 1,
+    id: 3,
     name: 'Quesos Extra Duros'
 },
 ]
